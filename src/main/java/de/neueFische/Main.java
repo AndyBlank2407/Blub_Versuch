74db1a0f86bd3c9210ff2abb74238244ca7a5dc3
andreas <andreas.blank.1@gmx.de>
2023-07-06 11:39:23 +0200

    Initial commit

---
+++ src/main/java/de/neueFische/Main.java
@@ -0,0 +1,25 @@
+package de.neueFische;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static int add(int a, int b){
+        int summe = a+b;
+
+        return summe;
+    }
+
+    public static boolean check(int a){
+
+        if(a > 100){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
---
+++ src/test/java/de/neueFische/MainTest.java
@@ -0,0 +1,16 @@
+package de.neueFische;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void check() {
+    }
+}
